0f03ce657d5116646dc1d26a644b2c97ead223f2
samolego <34912839+samolego@users.noreply.github.com>
2021-05-03 15:21:34 +0200

    Exclude MISC group

--- common/src/main/java/org/samo_lego/mobdisguises/mixin/ServerWorldMixin_SpawnDisguiser.java
+++ common/src/main/java/org/samo_lego/mobdisguises/mixin/ServerWorldMixin_SpawnDisguiser.java
@@ -2,6 +2,7 @@ package org.samo_lego.mobdisguises.mixin;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.registry.Registry;
@@ -12,21 +13,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.nucleoid.disguiselib.casts.EntityDisguise;
 
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 
 @Mixin(ServerWorld.class)
 public abstract class ServerWorldMixin_SpawnDisguiser {
 
-    private final Random RANDOM = new Random();
     private final List<EntityType<?>> DISGUISE_POSSIBILITIES = Registry.ENTITY_TYPE
             .stream()
             .filter(type ->
                     type.isSummonable() &&
-                    !type.equals(EntityType.ITEM) &&
-                    !type.equals(EntityType.FISHING_BOBBER) &&
-                    !type.equals(EntityType.PAINTING) &&
-                    !type.equals(EntityType.ITEM_FRAME)
+                    !type.getSpawnGroup().equals(SpawnGroup.MISC)
             )
             .collect(Collectors.toList());
 
@@ -40,9 +36,7 @@ public abstract class ServerWorldMixin_SpawnDisguiser {
     private void onEntitySpawn(Entity entity, CallbackInfoReturnable<Boolean> cir) {
         if(entity instanceof MobEntity && !((EntityDisguise) entity).isDisguised()) {
             try {
-                EntityType<?> disguise = this.DISGUISE_POSSIBILITIES.get(this.RANDOM.nextInt(DISGUISE_POSSIBILITIES.size()));
-
-                System.out.println(disguise);
+                EntityType<?> disguise = this.DISGUISE_POSSIBILITIES.get(((MobEntity) entity).getRandom().nextInt(DISGUISE_POSSIBILITIES.size()));
                 ((EntityDisguise) entity).disguiseAs(disguise);
             } catch(NullPointerException e) {
                 e.printStackTrace();
